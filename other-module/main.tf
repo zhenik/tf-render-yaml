cceb192b0f64e03167a75e7d10b43b60153178d0
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2022-04-04 13:03:07 +0200

    roll back usage to release 0.1.0

--- other-module/main.tf
+++ other-module/main.tf
@@ -1,8 +1,14 @@
 module "yaml-render" {
+# https://www.terraform.io/language/modules/sources#modules-in-package-sub-directories
+#  source = "git@github.com:zhenik/tf-render-yaml.git//terraform-template-vector-config?ref=0.2.0"
   source = "git@github.com:zhenik/tf-render-yaml.git//terraform-template-vector-config?ref=0.1.0"
 
   helm_conf = {
     aadpodidbinding = "wwwaaaaat"
   }
 }
+
+#output "rendered_helm_config" {
+#  value = module.yaml-render.rendered
+#}
 
